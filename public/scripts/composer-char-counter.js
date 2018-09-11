848c458f44c680a84e5772b88e4895cbb28e6a05
Alfred Law <alfred.lawsn@gmail.com>
2018-09-11 22:52:57 +0000

    finished first w3d1 and half of w3d2 assignments, up to dynamic tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+// Makes sure the DOM is ready to be manipulated with JQuery
+$( document ).ready(function() {
+    console.log( "Document loaded, ready for JQuery manipulation" );
+});
+
+
+$(document).ready(function() {
+  $("#text-box").on("input", function() {
+    let txtCount = 140 - this.value.length;
+    //console.log(txtCount);
+    $(this).siblings(".counter").text(txtCount);
+    if (txtCount < 0) {
+      $(this).siblings(".counter").css('color', 'red');
+    } else {
+      $(this).siblings(".counter").css('color', 'black');
+    }
+  });
+});
