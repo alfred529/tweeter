9010f2e1fe87a0e9c8eebbcfa4fbe960e754c988
Alfred Law <alfred.lawsn@gmail.com>
2018-09-12 20:31:11 +0000

    finished up to displaying error message using jQuery instead of standard alert

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,54 +5,6 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
-
 
 
 
@@ -62,12 +14,12 @@ function createTweetElement(tweetData){
   const createdDate = new Date(tweetData.created_at).toLocaleTimeString();
   // creates a var header, to be placed in the article using the following HTML
   const header = `<header class="article-tweet-header">
-                    <img class="profile-pic" src="${tweetData.user.avatars.small}">
-                    <h2 class="article-header-text">${tweetData.user.name}</h2> <p class="user-handle">${tweetData.user.handle}</p>
+                    <img class="profile-pic" src="${escape(tweetData.user.avatars.small)}">
+                    <h2 class="article-header-text">${escape(tweetData.user.name)}</h2> <p class="user-handle">${tweetData.user.handle}</p>
                   </header>`
-  const body = `<p class="article-content-text">${tweetData.content.text}</h4>`
+  const body = `<p class="article-content-text">${escape(tweetData.content.text)}</h4>`
   const footer = `<footer>
-                    <p class="article-tweet-footer">${createdDate}</p>
+                    <p class="article-tweet-footer">${escape(createdDate)}</p>
                     <p class="footer-icons">
                     <i class="fas fa-flag"></i>
                     <i class="fas fa-retweet"></i>
@@ -87,13 +39,75 @@ function createTweetElement(tweetData){
 function renderTweets(tweetArray) {
   // takes the array tweetArray, and for each entry (called tweet), appends (add to the end) it to the MAIN (in html) with the id tweets-container
   tweetArray.forEach(function(tweet) {
-    $("#tweets-container").append(createTweetElement(tweet));
+    $("#tweets-container").prepend(createTweetElement(tweet));
   });
 }
 
 
-//takes the data (currently in this file), when this file is loaded, put it through function renderTweets
+function postTweets() {
+
+  $( "form" ).on( "submit", function( event ) {
+    event.preventDefault();
+    console.log( escape($(this).serialize()) );
+    let $error = $('#tweet-error');
+    let $messageLength = $(this).find("textarea").val().length;
+    if ($messageLength < 1) {
+      // window.alert('You need to type something!')
+      $error.text("You need to type something!")
+      $('#tweet-error').show()
+    } else if ($messageLengthS > 140) {
+      // window.alert('Your post is too long!')
+      $error.text("Your post is too long!")
+      $('#tweet-error').show()
+    } else {
+      $.ajax('/tweets', {
+        data: $(this).serialize(),
+        method: 'POST'
+      })
+      $('#tweet-error').hide()
+      loadTweets();
+
+      console.log('Successfully sent to server');
+    }
+  })
+}
+
+// $( "img" )
+//   .error(function() {
+//     $( this ).attr( "src", "replacement.png" );
+//   })
+//   .attr( "src", "missing.png" );
+
+function loadTweets() {
+    $.ajax('/tweets', { method: 'GET' }).then(function(data) {
+      renderTweets(data);
+    })
+  }
+
+// prevents malicious code being posted
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
+
+// compose button toggles the new tweet window
+function newTweetToggle() {
+  $(".compose-button").click(function(){
+        $(".new-tweet").slideToggle("slow");
+        $("#text-box").focus();
+    });
+}
+
+
+
+
+
+//takes the data, when this file is loaded, put it through function renderTweets
 $(document).ready(function() {
-  renderTweets(data);
+  postTweets();
+  loadTweets();
+  newTweetToggle();
 })
 
